60003afe3cd13b8bd8de41da0bdd16effca997d7
Mark Paluch <mpaluch@vmware.com>
2022-12-01 15:23:47 +0100

    Upgrade to Vault 1.12.2.

--- test/run.sh
+++ test/run.sh
@@ -6,7 +6,7 @@ rm -f target/vault*
 mkdir -p target
 cd target
 
-VAULT_VER=0.8.3
+VAULT_VER=1.12.2
 UNAME=$(uname -s |  tr '[:upper:]' '[:lower:]')
 VAULT_ZIP="vault_${VAULT_VER}_${UNAME}_amd64.zip"
 
@@ -14,13 +14,13 @@ wget "https://releases.hashicorp.com/vault/${VAULT_VER}/${VAULT_ZIP}"
 unzip ${VAULT_ZIP}
 
 ./vault server --dev --dev-root-token-id="00000000-0000-0000-0000-000000000000" &
-sleep 1
+sleep 2
 
 export export VAULT_TOKEN="00000000-0000-0000-0000-000000000000"
 export VAULT_ADDR="http://127.0.0.1:8200"
 
-./vault write secret/gs-vault-config example.username=demouser example.password=demopassword
-./vault write secret/gs-vault-config/cloud example.username=clouduser example.password=cloudpassword
+./vault kv put secret/gs-vault-config example.username=demouser example.password=demopassword
+./vault kv put secret/gs-vault-config/cloud example.username=clouduser example.password=cloudpassword
 cd ..
 
 cd ../complete
