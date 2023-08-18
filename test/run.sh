7114c993e4e1d9688f1fea312ac170c7e017b0c6
Mark Paluch <mpaluch@vmware.com>
2023-08-18 11:25:09 +0200

    Increase wait timeout to prevent race conditions from a slow server startup

--- test/run.sh
+++ test/run.sh
@@ -14,7 +14,7 @@ wget "https://releases.hashicorp.com/vault/${VAULT_VER}/${VAULT_ZIP}"
 unzip ${VAULT_ZIP}
 
 ./vault server --dev --dev-root-token-id="00000000-0000-0000-0000-000000000000" &
-sleep 5
+sleep 10
 
 export export VAULT_TOKEN="00000000-0000-0000-0000-000000000000"
 export VAULT_ADDR="http://127.0.0.1:8200"
